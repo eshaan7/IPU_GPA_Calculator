1781442d28ae6f578763778119fefa5f3af373f6
eshaan7 <ishubansal2000@gmail.com>
2019-07-04 22:18:25 +0530

    pwa #1

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 This simple flask app is to help calculate GPA for GGSIPU University students. 
 ### https://ipugpacalculator.herokuapp.com/
 
-<p align="center"><img src="static/favicon.ico" width="300" height="300" border="1px" /></p>
+<p align="center"><img src="static/images/favicon.ico" width="300" height="300" border="1px" /></p>
--- app/application.py
+++ app/application.py
@@ -1,9 +1,23 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 app.secret_key = "66b58fafa6a470f26fd2adc9de14cef2"
 
+''' PWA Stuff '''
+# only trigger SSLify if the app is running on Heroku
+if 'DYNO' in os.environ: 
+	from flask_sslify import SSLify
+	sslify = SSLify(app)
+
+@app.route('/sw.js', methods=['GET'])
+def sw():
+    return app.send_static_file('sw.js')
+
+''' Routes/views ''' 
+
 @app.route('/', methods=['GET','POST'])
+@app.route('/home', methods=['GET','POST'])
 def index():
 	no_of_subjects = 14
 	if request.method == 'POST':
@@ -12,6 +26,8 @@ def index():
 		FinalGPA = gpa_calc(no_of_subjects, credits, grades)
 		return render_template('index.html', no_of_subjects=no_of_subjects, FinalGPA=FinalGPA)
 	return render_template('index.html', no_of_subjects=no_of_subjects)
+
+''' Utility functions '''
 
 def gpa_calc(no_of_subjects, credits, grades):
 	FinalGPA = 0
